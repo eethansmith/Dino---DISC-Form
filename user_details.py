2034b747728f03f6cdaa405f4aeff163a78cfa9f
psyes9 <ethan.a.smith@hotmail.co.uk>
2024-11-16 01:16:17 +0000

    chore: Update user_details.py to require email for payment success

--- user_details.py
+++ user_details.py
@@ -10,10 +10,12 @@ def input_user_details():
     st.write("""The fee for this Assessment will be **$0.00**.""")
     st.write("""For any queries, please contact dino.grif@gmail.com""")
     add_auth(required=True)
-    st.write("### Please fill in your details")
+    st.write
+    st.write("### Payment Successful! Please fill in your details")
     
     # Collect user details
     st.session_state.user_details['name'] = st.text_input("Name *", st.session_state.user_details['name'])
+    st.session_state.user_details['email'] = st.text_input("Email *", st.session_state.user_details['email'])
     # Add the date of birth input
     st.session_state.user_details['date_of_birth'] = st.date_input(
     "Date of Birth", 
@@ -27,8 +29,8 @@ def input_user_details():
     next_button_clicked = st.button("Next")
     
     # Check if the Next button was clicked without a name provided
-    if next_button_clicked and not st.session_state.user_details['name']:
-        st.error("Name is required to proceed.")
+    if next_button_clicked and not st.session_state.user_details['name'] or not st.session_state.user_details['email']:
+        st.error("Name and Email is required to proceed.")
     elif next_button_clicked:
         st.session_state.current_section = 1  # Move to the first question of the DISC assessment
         st.rerun()
