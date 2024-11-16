44ec7fa98d62b372a460bbf3ec3599b844ada622
psyes9 <ethan.a.smith@hotmail.co.uk>
2024-11-16 00:01:23 +0000

    chore: Add access_paywall function to paywall.py

--- paywall.py
+++ paywall.py
@@ -1,1 +1,7 @@
 import streamlit as st
+
+def access_paywall():
+    st.write(f"### DISC Personality Assessment ({progress})")
+
+    st.write("""To Proceed with the DISC personality assessment, please complete authentication and proceed to secure payment.""")
+    st.write("""The fee for this Assessment will be **$0.00**, payment processed my Stripe.""")
